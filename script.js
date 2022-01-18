c6d1d3948386a89f0f0c97eb752482c6dee982d1
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-18 23:26:13 +0800

    Added notes about Overview of Javascript

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+//A high-level overview of javascript
+
+/*
+Low-level - Developers has to manage resources manually
+
+High-level (JS, Python) - Developers does not have to worry, everything happens automatically
+
+Garbage-collection - Cleaning the memory so we don't have to manually.
+
+Interpreted / Just-in-time compiled - 
+
+Abstraction - Converted from machine code to human-readable code.
+
+
+Multi-paradigm - Javascript can become any of the paradigm below.
+
+Procedural Programming - linear way of coding
+
+Object-Oriented Programming (OOP) - 
+
+Functional Programming (FP) - 
+
+First-class functions - Simply treated as variables, can pass functions into other functions.
+
+Dynamic - No data type definitions, it is automatically changed whenever needed.
+
+Single-threaded - JS runs in one single thread, so it can only do one thing at a time.
+
+Concurrency Model - JS handles multiples tasks happening at the same time
+
+
+
+
+*/
