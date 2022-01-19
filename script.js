1080163e7e119966090d3cf599f3639704baf1ba
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-19 17:07:12 +0800

    Added notes about Execution Context and Call Stack

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//A high-level overview of javascript
+//A High-level Overview of Javascript
 
 /*
 Low-level - Developers has to manage resources manually
@@ -29,6 +29,42 @@ Dynamic - No data type definitions, it is automatically changed whenever needed.
 Single-threaded - JS runs in one single thread, so it can only do one thing at a time.
 
 Concurrency Model - JS handles multiples tasks happening at the same time
+
+
+//The Javascript Engine and Runtime
+
+Javascript Engine - A program that executes JS code
+
+Ex. V8 Engine -> Google Chrome / Node JS
+
+Call Stack - Where our code is executed
+
+Heap - Where objects are stored
+
+
+Compilation vs Interpretation
+
+Compilation - Entire code is converted into machine code and written to a binary file that can be executed by a computer
+
+Interpretation - runs through the source code and executes it line by line.
+
+Just-In-Time Compilation - Entire code is converted and executed immediately. 
+
+Optimization happens during execution but does not stop the program
+
+
+//Execution Contexts and the Call Stack
+
+Execution Context 
+- Environment in which a piece of JS is executed. Stores all the necessary info for some code to be executed.
+
+- Contains let, const, var, functions, scope chain, this keyword
+
+- Arrow functions has no this keyword and argument objects.
+
+Call Stack - place where execution contexts get stacked on top of each other, to keep track of where we are in the execution, like a map.
+
+
 
 
 
